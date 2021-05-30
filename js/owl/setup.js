1c9b789ac6cc05723fcfe410170294d2ac5c4fbd
Jonathas <jonathas-25@hotmail.com>
2021-05-30 20:36:55 -0300

    #7 Fim da aula 7
    Terminado a aula 7, colocando as imagens no carrossel . Explicando o Jquery do setup.js (Chamada do JQuery)

--- js/owl/setup.js
+++ js/owl/setup.js
@@ -1,7 +1,7 @@
 $('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:true,
+    loop:true, /*Para fazer o loop sim ou não*/ 
+    margin:10, /*Margem entre os filmes*/
+    nav:false, /*Botão de navegação*/
     responsive:{
         0:{
             items:1
